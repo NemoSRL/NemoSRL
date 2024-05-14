75d48bb0f32ef4a179c607a721938b45f17fdada
CundariAndrea <cundariandrea2002@gmail.com>
2024-05-14 19:11:34 +0200

    Eccezioni v 0.2
    Update di SogliaMinima

--- src/main/java/Eccezioni/SogliaMinimaMinoreDi0.java
+++ src/main/java/Eccezioni/SogliaMinimaMinoreDi0.java
@@ -2,6 +2,6 @@ package Eccezioni;
 
 public class SogliaMinimaMinoreDi0 extends Exception{
     public SogliaMinimaMinoreDi0(){
-        super ("Il valore di soglia minima non può essere minore o uguale a 0");
+        super ("Il valore di soglia minima non può essere minore di 0");
     }
 }
